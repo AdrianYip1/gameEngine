e03514b7523fdda44e741a664cac3cc7059c56af
Adrian Yip <yipadrian1119@gmail.com>
2026-04-29 17:17:08 -0400

    feat: first window created with glad and glfw

--- main.cpp
+++ main.cpp
@@ -3,17 +3,42 @@
 #include <GLFW/glfw3.h>
 #include <enginemath/vec3.hpp>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
 int main() {
 
-	enginemath::Vec3 a(1, 4, 6);
-	enginemath::Vec3 b(4, 6, 2);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	auto c = a + b;
-	auto d = a.cross(b);
-	auto e = a.dot(b);
 
-	std::cout << c.x << ", " << c.y << ", " << c.z << std::endl;
-	std::cout << d.x << ", " << d.y << ", " << d.z << std::endl;
-	std::cout << e << std::endl;
+	GLFWwindow* window = glfwCreateWindow(800, 600, "ganme engine", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initalize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+
 	return 0;
 }
+
+
