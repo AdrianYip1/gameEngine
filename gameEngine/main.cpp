f980f258b2234f78a8e1a76b59374381e27a6185
Adrian Yip <yipadrian1119@gmail.com>
2026-04-28 23:19:34 -0400

    Fix build system, CMake cleanup, and ignore build folders

--- gameEngine/main.cpp
+++ gameEngine/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <enginemath/vec3.hpp>
 
 int main() {
+
+	enginemath::Vec3 a(1, 4, 6);
+	enginemath::Vec3 b(4, 6, 2);
+
+	auto c = a + b;
+	auto d = a.cross(b);
+	auto e = a.dot(b);
+
+	std::cout << c.x << ", " << c.y << ", " << c.z << std::endl;
+	std::cout << d.x << ", " << d.y << ", " << d.z << std::endl;
+	std::cout << e << std::endl;
 	return 0;
 }
